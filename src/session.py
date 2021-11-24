35a0bd9b2cb6430da75244aa1da75fd153c12017
Fabio de Luca <pantano@gmail.com>
2021-11-24 14:34:57 +0100

    Added complete steps for authentication for testing

--- src/session.py
+++ src/session.py
@@ -4,6 +4,7 @@ import logging
 import json
 import uuid
 import datetime
+import time
 
 
 class Session:
@@ -20,8 +21,11 @@ class Session:
         self.__session_id = None
         self.__request_id = None
         self.__identifier = None
+        self.__authentication_info = None
+        self.__challenge_id = None
+        self.__challenge_type = None
 
-        #self.__source = source
+        # self.__source = source
         self.__access_config = access_config
 
         # ----------------------------------------------------------------------
@@ -40,8 +44,11 @@ class Session:
     def connect(self):
 
         # ----------------------------------------------------------------------
-        #   First step: Post the user information
+        #   2.1 OAuth2 Resource Owner Password Credentials Flow
         # ----------------------------------------------------------------------
+        response_body_json = None
+        response_headers_json = None
+
         url = self.__access_config["url_oauth_token"]
         payload = f"client_id={self.__client_id}&client_secret={self.__client_secret}&grant_type=password&username={self.__username}&password={self.__pin}"
         headers = {
@@ -51,33 +58,38 @@ class Session:
         }
 
         response = requests.request("POST", url, headers=headers, data=payload)
-        response_json = json.loads(response.text)
+        response_body_json = json.loads(response.text)
+        response_headers_json = dict(response.headers)
 
-        #print("---- 1 --------------------------------------")
-        #print(json.dumps(response_json, indent=4, sort_keys=True))
+        print("---- 2.1 --------------------------------------")
+        print(json.dumps(response_body_json, indent=4, sort_keys=True))
+        print(json.dumps(response_headers_json, indent=4, sort_keys=True))
 
         if response.status_code in [200, 201]:
-            self.__access_token = response_json["access_token"]
-            self.__refresh_token = response_json["refresh_token"]
+            self.__access_token = response_body_json["access_token"]
+            self.__refresh_token = response_body_json["refresh_token"]
 
-            result = response_json
+            result = response
             flag, level, message = M.get_status("API_Trade_Oauth_Success")
             if self.__logger is not None:
                 self.__logger.info(message)
         else:
-            result = response.text
+            result = response
             flag, level, message = M.get_status("API_Trade_Oauth_Error")
             if self.__logger is not None:
                 self.__logger.error(message)
             return result, flag, level, message
 
         # ----------------------------------------------------------------------
-        #   Second step
+        #   2.2 Session-Status
         # ----------------------------------------------------------------------
+        response_body_json = None
+        response_headers_json = None
+
         url = self.__access_config["url_session"]
 
-        self.__session_id = self.__get_session_id()
-        self.__request_id = self.__get_request_id()
+        self.__session_id = self.get_session_id()
+        self.__request_id = self.get_request_id()
 
         payload = {}
         headers = {
@@ -89,33 +101,36 @@ class Session:
         }
 
         response = requests.request("GET", url, headers=headers, data=payload)
-        response_json = json.loads(response.text)
 
-        # print("---- 2 --------------------------------------")
-        # print(response.text)
-        # print(response.status_code)
+        response_body_json = json.loads(response.text)
+        response_body_json = response_body_json[0]
+        response_headers_json = dict(response.headers)
 
-        # print(json.dumps(response_json, indent=4, sort_keys=True))
+        print("---- 2.2 --------------------------------------")
+        print(json.dumps(response_body_json, indent=4, sort_keys=True))
+        print(json.dumps(response_headers_json, indent=4, sort_keys=True))
 
         if response.status_code in [200, 201]:
-            self.__identifier = response_json["identifier"]
+            self.__identifier = response_body_json["identifier"]
 
-            result = response_json
+            result = response
             flag, level, message = M.get_status("API_Trade_Ident_Success")
             if self.__logger is not None:
                 self.__logger.info(message)
         else:
-            result = response.text
+            result = response
             flag, level, message = M.get_status("API_Trade_Ident_Error")
             if self.__logger is not None:
                 self.__logger.error(message)
             return result, flag, level, message
 
         # ----------------------------------------------------------------------
-        #   Third step - Validate session
+        #   2.3 System validation of session TAN
         # ----------------------------------------------------------------------
+        response_body_json = None
+        response_headers_json = None
+
         url = self.__access_config["url_session_validate"]
-        #url = "https://api.comdirect.de/api/session/clients/user/v1/sessions/F6D3F5F6BF4E4A758063E75135C120A6/validate"
         url = url.replace("[IDENTIFIER]", self.__identifier)
 
         payload = json.dumps({
@@ -127,37 +142,102 @@ class Session:
             'Accept': 'application/json',
             'Authorization': f'Bearer {self.__access_token}',
             'x-http-request-info': f'{{"clientRequestId":{{"sessionId":"{self.__session_id}","requestId":"{self.__request_id}"}}}}',
-            # 'x-http-request-info': '{"clientRequestId":{"sessionId":"34d0a0ba-3115-bcb0-3501-82619c482730","requestId":"691878198"}}',
             'Content-Type': 'application/json',
             # 'Cookie': 'qSession=22608551.eb2b00043f94633a8bb8b20'
         }
 
         response = requests.request("POST", url, headers=headers, data=payload)
-        response_json = json.loads(response.text)
+        response_body_json = json.loads(response.text)
+        response_headers_json = dict(response.headers)
 
-        print("---- 3 --------------------------------------")
-
-        print(json.dumps(response_json, indent=4, sort_keys=True))
+        print("---- 2.3 --------------------------------------")
+        print(json.dumps(response_body_json, indent=4, sort_keys=True))
+        print(json.dumps(response_headers_json, indent=4, sort_keys=True))
 
         if response.status_code in [200, 201]:
-            self.__identifier = response_json["identifier"]
+            info = response_headers_json["x-once-authentication-info"]
+            (self.__challenge_id,
+             self.__challenge_type,
+             self.__authentication_info) = self.get_challenge_info(info)
 
-            result = response_json
+            result = response
             flag, level, message = M.get_status("API_Trade_Validate_Success")
             if self.__logger is not None:
                 self.__logger.info(message)
         else:
-            result = response.text
+            result = response
             flag, level, message = M.get_status("API_Trade_Validate_Error")
             if self.__logger is not None:
                 self.__logger.error(message)
             return result, flag, level, message
 
-        return response_json
+        # ----------------------------------------------------------------------
+        #   2.4 Activation of a session TAN
+        # ----------------------------------------------------------------------
+        response_body_json = None
+        response_headers_json = None
+
+        url = self.__access_config["url_session_tan"]
+        url = url.replace("[IDENTIFIER]", self.__identifier)
+
+        payload = json.dumps({
+            "identifier": f"{self.__identifier}",
+            "sessionTanActive": True,
+            "activated2FA": True
+        })
+        headers = {
+            'Accept': 'application/json',
+            'Authorization': f'Bearer {self.__access_token}',
+            'x-http-request-info': f'{{"clientRequestId":{{"sessionId":"{self.__session_id}","requestId":"{self.__request_id}"}}}}',
+            'Content-Type': 'application/json',
+            'x-once-authentication-info': f'{{"id":"{self.__challenge_id}"}}',
+            'x-once-authentication': '',
+            # 'Cookie': 'qSession=22608551.ce1bae085773e7f8941cb9f'
+        }
+
+        response = requests.request(
+            "PATCH", url, headers=headers, data=payload)
+        response_body_json = json.loads(response.text)
+        response_headers_json = dict(response.headers)
+
+        print("---- 2.4 --------------------------------------")
+        print(json.dumps(response_body_json, indent=4, sort_keys=True))
+        print(json.dumps(response_headers_json, indent=4, sort_keys=True))
+
+        time.sleep(8)
+
+        # ----------------------------------------------------------------------
+        #   2.5 OAuth2 CD Secondary-Flow
+        # ----------------------------------------------------------------------
+        url = self.__access_config["url_oauth_token"]
+
+        payload = f"client_id={self.__client_id}&client_secret={self.__client_secret}&grant_type=cd_secondary&token={self.__access_token}"
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Accept': 'application/json',
+            # 'Cookie': 'qSession=22608551.ce1bae085773e7f8941cb9f'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+        response_body_json = json.loads(response.text)
+        response_headers_json = dict(response.headers)
+
+        print("---- 2.5 --------------------------------------")
+        print(json.dumps(response_body_json, indent=4, sort_keys=True))
+        print(json.dumps(response_headers_json, indent=4, sort_keys=True))
+
+        return result, flag, level, message
 
     def get_session_id(self):
-        self.__session_id = uuid.uuid4()
+        return uuid.uuid4()
 
     def get_request_id(self):
-        self.__request_id = datetime.datetime.now(
+        return datetime.datetime.now(
             datetime.timezone.utc).strftime("%Y%m%d%H%M%S%f")
+
+    def get_challenge_info(self, info):
+        authentication_info = json.loads(info)
+        challenge_id = authentication_info["id"]
+        challenge_type = authentication_info["typ"]
+
+        return challenge_id, challenge_type, authentication_info
