4b3ffd36d41eb5c7bda0c950de7521db38502ecb
Fabio de Luca <pantano@gmail.com>
2022-01-02 15:05:36 +0100

    Added support to attachement on MailGun and added Jinja to Requirments

--- src/lib/communication/mail.py
+++ src/lib/communication/mail.py
@@ -1,11 +1,14 @@
 import requests
+from pathlib import Path
 from src.lib import messages as M
 
 
 class Mail:
 
     def send_email(self, subject: str,
-                   body_html: str, body_plain: str = ""):
+                   body_html: str,
+                   body_plain: str = "",
+                   attachment: list[Path] = None):
         """Sends an email using the MailGun API.
         """
         url = self.access_config["email"]["URL_BASE"] + \
@@ -17,14 +20,26 @@ class Mail:
         to_email = self.access_userdata["email"]["to_email"]
         from_email = self.access_userdata["email"]["from_email"]
 
+        if attachment is not None:
+            if isinstance(attachment, Path):
+                attachment = [attachment]
+            attachment_request = []
+            for item in attachment:
+                if item.exists():
+                    outfile = open(item, 'rb').read()
+                    attachment_request.append(
+                        ("attachment", (item.name, outfile)))
+        else:
+            attachment_request = None
+
         try:
             # disable ssl warning
             # requests.packages.urllib3.disable_warnings()
             # context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
             # context.verify_mode = ssl.CERT_NONE
-
             r = requests.post(url,
                               auth=("api", apikey),
+                              files=attachment_request,
                               data={"from": from_email,
                                     "to": to_email,
                                     "subject": subject,
