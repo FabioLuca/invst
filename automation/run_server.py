536110785c916f52de941ff6825920be7dad9d88
Fabio de Luca <pantano@gmail.com>
2022-01-08 21:03:37 +0100

    Update run_server.py

--- automation/run_server.py
+++ automation/run_server.py
@@ -5,16 +5,19 @@ from automation import run_analysis
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.get("/")
 def basic():
     return "Running"
 
 
-@app.route("/run")
+@app.get("/run")
 def call_run_analysis():
     return run_analysis.run_analysis()
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="localhost",  # "0.0.0.0",
-            port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True,
+            host="localhost",
+            port=8080)
+
+    # port=int(os.environ.get("PORT", 8080)))
