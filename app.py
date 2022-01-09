dc716981fcb0b1c5d407075206e3ec6eb0b09a43
Fabio de Luca <pantano@gmail.com>
2022-01-09 06:48:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import glob
 from automation import run_analysis
 from automation import comdirect_status_update
 
@@ -15,13 +16,20 @@ def test():
     return "It's Running a Test"
 
 
-@app.route("/analysis")
+@app.route("/list")
+def list_files():
+    list_files_folder = glob.glob("/*")
+    list_string = ", ".join(list_files_folder)
+    return list_string
+
+
+@ app.route("/analysis")
 def call_run_analysis():
     run_analysis.run_analysis()
     return "Running market analysis"
 
 
-@app.route("/update")
+@ app.route("/update")
 def call_run_update():
     comdirect_status_update.run_update()
     return "Running update from Comdirect"
